a056e2fe0db764250041564dae13fd43bf943aa7
Brayan0813 <brayansanchez0813@gmail.com>
2024-04-04 23:45:48 -0500

    base de datos

--- libreria/urls.py
+++ libreria/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns =[
     path('',views.inicio, name='inicio'),
     path('nosotros',views.nosotros, name='nosotros'),
-]
+    path('libros',views.libros, name='libros'),
+    path('libros/crear',views.crear, name='crear'),
+    path('libros/editar',views.editar, name='editar'),
+
+
+]   
--- libreria/views.py
+++ libreria/views.py
@@ -3,7 +3,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def inicio(request):
-    return HttpResponse("<h1>Bienvenido a la Biblioteca</h1>")
+    return render(request,'paginas/inicio.html')
 
 def nosotros(request):
-    return render(request, 'paginas/nosotros.html')
+    return render(request,'paginas/nosotros.html')
+
+def libros(request):
+    return render(request,'libros/index.html')
+
+def crear(request):
+    return render(request,'libros/crear.html')
+
+def editar(request):
+    return render(request,'libros/editar.html')
