1a8797f27d757b72a7a839fdd87894e9431f2bea
Brayan0813 <brayansanchez0813@gmail.com>
2024-04-04 22:38:52 -0500

    editar y organizar

--- libreria/views.py
+++ libreria/views.py
@@ -21,9 +21,12 @@ def crear(request):
         return redirect('libros')
     return render(request,'libros/crear.html',{'formulario':formulario})
 
-def editar(request, id):
+def editar(request,id):
     libro=Libro.objects.get(id=id)
     formulario=LibroForm(request.POST or None,request.FILES or None, instance=libro)
+    if formulario.is_valid()and request.POST:
+        formulario.save()
+        return redirect('libros')
     return render(request,'libros/editar.html',{'formulario':formulario})
 
 def eliminar(request,id):
